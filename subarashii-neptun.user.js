69223c22919e51f9252094cec921185b5b31345e
Domonkos Nagy <nagydomonkos1@gmail.com>
2021-02-28 18:39:39 +0100

    sdf

--- subarashii-neptun.user.js
+++ subarashii-neptun.user.js
@@ -27,12 +27,9 @@
     $("#span_changeproject").parent().show();
 
     //DDLC
-    for (var i = 0, max = ss.length; i < max; i++) {
-        console.log(ss[i].href);
-        if (typeof ss[i].href == 'string' && ss[i].href.includes("App_Themes/Skin_Neptun_Pink/Skin_Neptun_Pink.css")) {
-            document.querySelector('.main_header_r').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/ddlc/header_right.jpg")';
-            document.querySelector('.main_header_m').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/ddlc/header_mid.jpg")';
-            document.querySelector('.main_header_l').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/ddlc/header_left.jpg")';
-        };
-    }
+    if (ss[16].href.includes("App_Themes/Skin_Neptun_Pink")) {
+        document.querySelector('.main_header_r').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/ddlc/header_right.jpg")';
+        document.querySelector('.main_header_m').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/ddlc/header_mid.jpg")';
+        document.querySelector('.main_header_l').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/ddlc/header_left.jpg")';
+
 })();
