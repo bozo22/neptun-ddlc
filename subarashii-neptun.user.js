e27332c793777e3a6734e6869b2bc7631bd671b1
Domonkos Nagy <nagydomonkos1@gmail.com>
2021-02-28 21:57:26 +0100

    5.0: A Naruto Incidens

--- subarashii-neptun.user.js
+++ subarashii-neptun.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         subarashii-neptun
 // @namespace    http://tampermonkey.net/
-// @version      0.4.2
+// @version      0.5
 // @description  gigantikus anime csöcsök
 // @author       You
 // @downloadURL    https://github.com/bozo22/subarashii-neptun/releases/latest/download/subarashii-neptun.user.js
@@ -23,21 +23,29 @@
 
     let skin = document.styleSheets[16].href;
 
-    //Neptun PowerUp kompatibilitás
-    $("#panHeader, #panCloseHeader").show();
-    $("#span_changeproject").parent().show();
-
     //DDLC skin
     if (skin.includes("App_Themes/Skin_Neptun_Pink")) {
-        document.querySelector('.main_header_r').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/ddlc/header_right.jpg")';
-        document.querySelector('.main_header_m').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/header_mid.jpg")';
-        document.querySelector('.main_header_l').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/ddlc/header_left.jpg")';
+        document.querySelector('.main_header_r').style.background='url("https://raw.githubusercontent.com/bozo22/subarashii-neptun/main/images/ddlc/header_right.jpg")';
+        document.querySelector('.main_header_m').style.background='url("https://raw.githubusercontent.com/bozo22/subarashii-neptun/main/images/header_mid.jpg")';
+        document.querySelector('.main_header_l').style.background='url("https://raw.githubusercontent.com/bozo22/subarashii-neptun/main/images/ddlc/header_left.jpg")';
     }
 
     //Evangelion skin
     if (skin.includes("App_Themes/Skin_Neptun_GFX")) {
-        document.querySelector('.main_header_r').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/eva/header_right.jpg")';
-        document.querySelector('.main_header_m').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/header_mid.jpg")';
-        document.querySelector('.main_header_l').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/eva/header_left.jpg")';
+        document.querySelector('.main_header_r').style.background='url("https://raw.githubusercontent.com/bozo22/subarashii-neptun/main/images/eva/header_right.jpg")';
+        document.querySelector('.main_header_m').style.background='url("https://raw.githubusercontent.com/bozo22/subarashii-neptun/main/images/header_mid.jpg")';
+        document.querySelector('.main_header_l').style.background='url("https://raw.githubusercontent.com/bozo22/subarashii-neptun/main/images/eva/header_left.jpg")';
     }
+
+    //Naruto skin
+    if (skin.includes("App_Themes/Skin_Neptun_Orange")) {
+        document.querySelector('.main_header_r').style.background='url("https://raw.githubusercontent.com/bozo22/subarashii-neptun/main/images/naruto/header_right.jpg")';
+        document.querySelector('.main_header_m').style.background='url("https://raw.githubusercontent.com/bozo22/subarashii-neptun/main/images/header_mid.jpg")';
+        document.querySelector('.main_header_l').style.background='url("https://raw.githubusercontent.com/bozo22/subarashii-neptun/main/images/naruto/header_left.jpg")';
+    }
+
+    //Neptun PowerUp kompatibilitás
+    $("#panHeader, #panCloseHeader").show();
+    $("#span_changeproject").parent().show();
+    
 })();
