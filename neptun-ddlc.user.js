b980506aae7f3949f252bf8085e26ad5a9cdced1
bozo22 <75812823+bozo22@users.noreply.github.com>
2021-02-28 17:09:10 +0100

    Update neptun-ddlc.user.js

--- neptun-ddlc.user.js
+++ neptun-ddlc.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://hallgato1.neptun.elte.hu/*
+// @match        https://hallgato*.neptun.elte.hu/*
 // @icon         https://www.google.com/s2/favicons?domain=elte.hu
 // @grant        none
 // ==/UserScript==
