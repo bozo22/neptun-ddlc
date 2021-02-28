f0d821ce603c1a121347492f756a4b9c6a978be1
Domonkos Nagy <nagydomonkos1@gmail.com>
2021-02-28 18:42:39 +0100

    asd

--- subarashii-neptun.user.js
+++ subarashii-neptun.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         subarashii-neptun
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.4.1
 // @description  gigantikus anime csöcsök
 // @author       You
 // @downloadURL    https://github.com/bozo22/subarashii-neptun/releases/latest/download/subarashii-neptun.user.js
@@ -20,16 +20,16 @@
 (function() {
     'use strict';
 
-    let ss = document.styleSheets;
+    let skin = document.styleSheets[16].href;
 
     //Neptun PowerUp kompatibilitás
     $("#panHeader, #panCloseHeader").show();
     $("#span_changeproject").parent().show();
 
     //DDLC
-    if (ss[16].href.includes("App_Themes/Skin_Neptun_Pink")) {
+    if (skin.includes("App_Themes/Skin_Neptun_Pink")) {
         document.querySelector('.main_header_r').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/ddlc/header_right.jpg")';
         document.querySelector('.main_header_m').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/ddlc/header_mid.jpg")';
         document.querySelector('.main_header_l').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/ddlc/header_left.jpg")';
-
+    }
 })();
