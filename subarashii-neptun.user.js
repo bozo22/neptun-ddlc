3e584f81e0e4add5b4c677ea1097d043234e874f
Domonkos Nagy <nagydomonkos1@gmail.com>
2021-02-28 19:35:10 +0100

    added eva skin

--- subarashii-neptun.user.js
+++ subarashii-neptun.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         subarashii-neptun
 // @namespace    http://tampermonkey.net/
-// @version      0.4.1
+// @version      0.4.2
 // @description  gigantikus anime csöcsök
 // @author       You
 // @downloadURL    https://github.com/bozo22/subarashii-neptun/releases/latest/download/subarashii-neptun.user.js
+// @updateURL      https://github.com/bozo22/subarashii-neptun/releases/latest/download/subarashii-neptun.user.js
 // @include        https://*neptun*/*hallgato*/*
 // @include        https://*neptun*/*Hallgatoi*/*
 // @include        https://*neptun*/*oktato*/*
@@ -26,10 +27,17 @@
     $("#panHeader, #panCloseHeader").show();
     $("#span_changeproject").parent().show();
 
-    //DDLC
+    //DDLC skin
     if (skin.includes("App_Themes/Skin_Neptun_Pink")) {
         document.querySelector('.main_header_r').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/ddlc/header_right.jpg")';
-        document.querySelector('.main_header_m').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/ddlc/header_mid.jpg")';
+        document.querySelector('.main_header_m').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/header_mid.jpg")';
         document.querySelector('.main_header_l').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/ddlc/header_left.jpg")';
+    }
+
+    //Evangelion skin
+    if (skin.includes("App_Themes/Skin_Neptun_GFX")) {
+        document.querySelector('.main_header_r').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/eva/header_right.jpg")';
+        document.querySelector('.main_header_m').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/header_mid.jpg")';
+        document.querySelector('.main_header_l').style.background='url("https://raw.githubusercontent.com/bozo22/neptun-ddlc/main/images/eva/header_left.jpg")';
     }
 })();
